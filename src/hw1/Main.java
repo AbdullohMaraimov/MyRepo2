5ffab4b18a24cecd346d654fa837779c70e34261
Abdulloh Maraimov <laptop.2022@gmail.com>
2023-10-20 10:56:41 +0500

    First Commit

---
+++ src/hw1/Main.java
@@ -0,0 +1,11 @@
+package hw1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int numbersCount = RegexClass.getNumbersCount("Bir1 ikki2 uch3 to'rtbesh45 olti6 yettisakkiz78");
+        System.out.println(numbersCount);
+
+
+    }
+}
